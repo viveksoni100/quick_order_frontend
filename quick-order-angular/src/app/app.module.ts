43d73edc63c0909a55452f7c0230a374a38bee29
viveksoni100 <viveksoni100@gmail.com>
2025-08-16 00:59:38 +0530

    .

--- quick-order-angular/src/app/app.module.ts
+++ quick-order-angular/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
-import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -10,7 +9,6 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule  
 ],
   providers: []
