9aadd1d77c0db6d50b4130c4e7045ecd3bf0bdaf
viveksoni100 <viveksoni100@gmail.com>
2025-08-17 17:54:51 +0530

    great happy cart added

--- quick-order-angular/src/app/outlet-menu/outlet-menu.component.ts
+++ quick-order-angular/src/app/outlet-menu/outlet-menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { API_DOMAIN } from '../constants';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-outlet-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './outlet-menu.component.html',
   styleUrl: './outlet-menu.component.css'
 })
@@ -12,10 +15,69 @@ export class OutletMenuComponent {
   outletId: string | null = null;
   menuId: string | null = null;
 
-  constructor(private route: ActivatedRoute) {
+  items: any[] = [];
+  filteredItems: any[] = [];
+  uniqueCategories: string[] = [];
+  selectedCategory: string | null = null;
+  cart: any[] = [];
+  cartTotal: number = 0;
+  showCheckout: boolean = false;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.queryParamMap.subscribe(params => {
       this.outletId = params.get('outletId');
       this.menuId = params.get('menuId');
     });
   }
+
+  ngOnInit() {
+    this.fetchItems();
+  }
+
+  fetchItems() {
+    const menuId = this.menuId;
+    if (!menuId) return;
+    this.http.get<any[]>(`${API_DOMAIN}api/outlet/getAllItemsByMenuId/${menuId}`).subscribe(data => {
+      this.items = data;
+      this.uniqueCategories = [...new Set(this.items.map(i => i.category))];
+      this.filterItems();
+    });
+  }
+
+  selectCategory(cat: string | null) {
+  this.selectedCategory = cat;
+  this.filterItems();
+}
+
+filterItems() {
+  this.filteredItems = this.selectedCategory
+    ? this.items.filter(i => i.category === this.selectedCategory)
+    : this.items;
+}
+
+addToCart(item: any) {
+  const found = this.cart.find(ci => ci.id === item.id);
+  if (found) {
+    found.quantity += 1;
+  } else {
+    this.cart.push({ ...item, quantity: 1 });
+  }
+  this.updateCartTotal();
+}
+
+removeFromCart(item: any) {
+  this.cart = this.cart.filter(ci => ci.id !== item.id);
+  this.updateCartTotal();
+}
+
+updateCartTotal() {
+  this.cartTotal = this.cart.reduce((sum, i) => sum + i.price * i.quantity, 0);
+}
+
+checkout() {
+  this.cart = [];
+  this.cartTotal = 0;
+  this.showCheckout = true;
+  setTimeout(() => this.showCheckout = false, 3000);
+}
 }
